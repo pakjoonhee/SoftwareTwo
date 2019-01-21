b02a16599eb0c0c5b59cf26112ca886cd40b6b2e
Joonhee <pakjoonhee90@gmail.com>
2019-01-20 21:40:42 -0800

    edit work

--- src/AppointmentDetails/AddAppointmentDetailsController.java
+++ src/AppointmentDetails/AddAppointmentDetailsController.java
@@ -26,10 +26,21 @@ import static software.ii.project.DBConnection.conn;
 public class AddAppointmentDetailsController implements Initializable {
     @FXML ComboBox<String> timesComboBox;
     @FXML ComboBox<String> consultantNamesComboBox;
+    @FXML ComboBox<String> months;
+    @FXML ComboBox<String> days;
+    @FXML ComboBox<String> years;
+    
     ObservableList<String> times = FXCollections.observableArrayList();
     ObservableList<String> consultantNames = FXCollections.observableArrayList();
+    ObservableList<String> monthList = FXCollections.observableArrayList();
+    ObservableList<String> dayList = FXCollections.observableArrayList();
+    ObservableList<String> yearList = FXCollections.observableArrayList();
+    
     private String selectedTime;
     private String selectedConsultant;
+    private String selectedMonth;
+    private String selectedDay;
+    private String selectedYear;
     @FXML TextField appointmentDate;
     @FXML TextField appointmentType;
     private Integer customerID;
@@ -38,10 +49,21 @@ public class AddAppointmentDetailsController implements Initializable {
     void handleTimeBox(ActionEvent event) {
         selectedTime = timesComboBox.getValue();
     }
-    
     @FXML
     void handleConsultantBox(ActionEvent event) {
         selectedConsultant = consultantNamesComboBox.getValue();
+    }
+    @FXML
+    void handleMonthBox(ActionEvent event) {
+        selectedMonth = months.getValue();
+    }
+    @FXML
+    void handleDayBox(ActionEvent event) {
+        selectedDay = days.getValue();
+    }
+    @FXML
+    void handleYearBox(ActionEvent event) {
+        selectedYear = years.getValue();
     }
     
     public void initCustomerID(Integer customerID) {
@@ -69,15 +91,24 @@ public class AddAppointmentDetailsController implements Initializable {
             times.add(strDate);
         }
 
-        timesComboBox.setItems(times);
         
         consultantNames.addAll("Consultant 1", "Consultant 2", "Consultant 3");
+        monthList.addAll("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12");
+        dayList.addAll("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", 
+                       "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31");
+        yearList.addAll("2019", "2020", "2021");
+        
+        timesComboBox.setItems(times);
         consultantNamesComboBox.setItems(consultantNames);
+        months.setItems(monthList);
+        days.setItems(dayList);
+        years.setItems(yearList);
     }
 
     public void saveFunction(ActionEvent event) throws SQLException, IOException {
+        String date = selectedMonth + "-" + selectedDay + "-" + selectedYear;
         Statement statement = conn.createStatement();
-        String sqlStatement = ("INSERT INTO `appointments_tbl`(CustomerID, AppointmentDate, AppointmentTime, AppointmentType, CustomerName) VALUE ('"+customerID+"','"+appointmentDate.getText()+"','"+selectedTime+"','"+appointmentType.getText()+"','"+selectedConsultant+"')");
+        String sqlStatement = ("INSERT INTO `appointments_tbl`(CustomerID, AppointmentDate, AppointmentTime, AppointmentType, CustomerName) VALUE ('"+customerID+"','"+date+"','"+selectedTime+"','"+appointmentType.getText()+"','"+selectedConsultant+"')");
         statement.executeUpdate(sqlStatement);
         
         Parent tableViewParent = FXMLLoader.load(getClass().getResource("/MainScreen/MainScreen.fxml"));
--- src/AppointmentDetails/EditAppointmentDetailsController.java
+++ src/AppointmentDetails/EditAppointmentDetailsController.java
@@ -29,11 +29,21 @@ import static software.ii.project.DBConnection.conn;
 public class EditAppointmentDetailsController implements Initializable {
     @FXML ComboBox<String> timesComboBox;
     @FXML ComboBox<String> consultantNamesComboBox;
+    @FXML ComboBox<String> months;
+    @FXML ComboBox<String> days;
+    @FXML ComboBox<String> years;
+    
     ObservableList<String> times = FXCollections.observableArrayList();
     ObservableList<String> consultantNames = FXCollections.observableArrayList();
+    ObservableList<String> monthList = FXCollections.observableArrayList();
+    ObservableList<String> dayList = FXCollections.observableArrayList();
+    ObservableList<String> yearList = FXCollections.observableArrayList();
+    
     private String selectedTime;
     private String selectedConsultant;
-    @FXML TextField appointmentDate;
+    private String selectedMonth;
+    private String selectedDay;
+    private String selectedYear;
     @FXML TextField appointmentType;
     private Appointment appointment;
     
@@ -41,10 +51,21 @@ public class EditAppointmentDetailsController implements Initializable {
     void handleTimeBox(ActionEvent event) {
         selectedTime = timesComboBox.getValue();
     }
-    
     @FXML
     void handleConsultantBox(ActionEvent event) {
         selectedConsultant = consultantNamesComboBox.getValue();
+    }
+   @FXML
+    void handleMonthBox(ActionEvent event) {
+        selectedMonth = months.getValue();
+    }
+    @FXML
+    void handleDayBox(ActionEvent event) {
+        selectedDay = days.getValue();
+    }
+    @FXML
+    void handleYearBox(ActionEvent event) {
+        selectedYear = years.getValue();
     }
     
     @Override
@@ -69,20 +90,32 @@ public class EditAppointmentDetailsController implements Initializable {
             times.add(strDate);
         }
 
-        timesComboBox.setItems(times);
         
         consultantNames.addAll("Consultant 1", "Consultant 2", "Consultant 3");
+        monthList.addAll("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12");
+        dayList.addAll("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", 
+                       "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31");
+        yearList.addAll("2019", "2020", "2021");
+        
+        timesComboBox.setItems(times);
         consultantNamesComboBox.setItems(consultantNames);
+        months.setItems(monthList);
+        days.setItems(dayList);
+        years.setItems(yearList);
     }    
     
     public void initAppointmentData(Appointment appointment, Integer rowNumber) {
         this.appointment = appointment;
+        String[] splitDate = appointment.getAppointmentDate().split("-");
+        selectedMonth = splitDate[0];
+        selectedDay = splitDate[1];
+        selectedYear = splitDate[2];
+        months.getSelectionModel().select(splitDate[0]);
+        days.getSelectionModel().select(splitDate[1]);
+        years.getSelectionModel().select(splitDate[2]);
         timesComboBox.getSelectionModel().select(appointment.getAppointmentTime());
         selectedTime = appointment.getAppointmentTime();
-        
-        appointmentDate.setText(appointment.getAppointmentDate());
         appointmentType.setText(appointment.getAppointmentType());
-        
         consultantNamesComboBox.getSelectionModel().select(appointment.getConsultantName());
         selectedConsultant = appointment.getConsultantName();
      } 
@@ -99,12 +132,14 @@ public class EditAppointmentDetailsController implements Initializable {
     }
     
     public void saveEditedAppointmentData(ActionEvent event) throws SQLException, IOException {
+        String date = selectedMonth + "-" + selectedDay + "-" + selectedYear;
+        System.out.print(date);
         try
         {
             PreparedStatement ps = conn.prepareStatement(
               "UPDATE appointments_tbl SET AppointmentDate = ?, AppointmentTime = ?, AppointmentType = ?, CustomerName = ? WHERE KeyID = ?");
 
-            ps.setString(1,appointmentDate.getText());
+            ps.setString(1,date);
             ps.setString(2,selectedTime);
             ps.setString(3,appointmentType.getText());
             ps.setString(4,selectedConsultant);
